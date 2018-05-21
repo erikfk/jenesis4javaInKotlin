1871ad54ad563a72cec5278d4f8eba93e6cb95c1
ErikFranzK <erik.koerber@gmail.com>
2018-05-21 08:19:54 +0200

    Improved documentation

--- src/main/java/net/sourceforge/jenesis4java/j4jik/JKeywords.kt
+++ src/main/java/net/sourceforge/jenesis4java/j4jik/JKeywords.kt
@@ -1,6 +1,20 @@
 package net.sourceforge.jenesis4java.j4jik
 
+/**
+ * Defines Java keywords that are used in various contexts.
+ * It's not yet clear if that the right way to handle the issue of Java keywords
+ * - that should not be handled as Strings all over the place I think.
+ *
+ *  @param[keyword] the text of the Java keyword.
+ * @constructor creates a new Java keyword enum with its texts
+ */
 enum class JKeywords(val keyword: String) {
+    /**
+     * Java class keyword.
+     */
     CLASS("class"),
+    /**
+     * Java rxtends keyword.
+     */
     EXTENDS("extends")
 }
--- src/main/java/net/sourceforge/jenesis4java/j4jik/JWildcard.kt
+++ src/main/java/net/sourceforge/jenesis4java/j4jik/JWildcard.kt
@@ -3,12 +3,19 @@ package net.sourceforge.jenesis4java.j4jik
 /**
  * Models a typed argument wildcard.
  * See JLS 10 §4.5.1
+ *
+ * @param[type] type of wildcard bounds `extends` or `super`
+ * @param[referenceType] reference type of wildcard bounds.
  */
 class JWildcard private constructor(val type: JKeywords, val referenceType: String) {
     /**
      * Offers factory methods to create sensible [JWildcard] instances.
      */
     companion object {
+        /**
+         * Creates a wildcard declaration with extends, e.g.
+         * `? extends ReferenceType`
+         */
         fun extends(referenceType: String): JWildcard {
             return JWildcard(JKeywords.EXTENDS, referenceType)
         }
