0ea45b2749e9bac3ed49bb20158489fab2f7f105
ErikFranzK <erik.koerber@gmail.com>
2018-05-30 15:40:15 +0200

    Minor dokka improvements.

--- src/main/java/net/sourceforge/jenesis4java/j4jik/InterfaceModifiers.kt
+++ src/main/java/net/sourceforge/jenesis4java/j4jik/InterfaceModifiers.kt
@@ -1,21 +1,25 @@
 package net.sourceforge.jenesis4java.j4jik
 
 /**
- * Models a group of interface modifiers )for an interface etc.).
+ * Models a group of interface modifiers (for an interface etc.).
  *
  * See JLS 10 §9.1.1
  */
 class InterfaceModifiers {
     /**
-    * optional annotations among interface modifiers.
+    * List of annotations of the [InterfaceModifiers] instance.
      */
     private val annotations: MutableList<Annotation> = ArrayList()
 
     /**
-    * Set containing the interface modifiers other than the annotations
+    * Set containing the interface modifiers other than the annotations.
      */
     val modifierKeywords: MutableSet<InterfaceModifierKeyword> = HashSet()
 
+    /**
+     * Adds the specified [annotation] to the list of annotations of annotations of this
+     * [InterfaceModifiers] instance.
+     */
     fun add(annotation: Annotation) {
         this.annotations.add(annotation)
     }
